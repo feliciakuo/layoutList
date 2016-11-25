4ac6276ec6a4b5415aa508c858de70cd72f03cb4
felicia <midori17zi@gmail.com>
2016-11-25 12:37:55 +0800

    fetch

--- src/actions/action.js
+++ src/actions/action.js
@@ -1,3 +1,5 @@
+import 'whatwg-fetch'
+
 /*
 * action type
 */
@@ -8,6 +10,10 @@ export const REVERT_ITEM = 'REVERT_ITEM'
 export const UPDATE_EDIT_ID = 'UPDATE_EDIT_ID'
 export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER'
 export const SET_ADDFORM_SWITCH = 'SET_ADDFORM_SWITCH'
+export const SELECT_SUBREDDIT = 'SELECT_SUBREDDIT'
+export const INVALDATE_SUBREDDIT = 'INVALDATE_SUBREDDIT'
+export const REQUEST_POSTS = 'REQUEST_POSTS'
+export const RECEIVE_POSTS = 'RECEIVE_POSTS'
 
 /*
 * 其他常數
@@ -58,4 +64,50 @@ export function setVisibilityFilter(filter) {
 
 export function setAddFormSwitch(filter) {
   return { type: SET_ADDFORM_SWITCH, filter }
+}
+
+// 載入資料
+export function selectSubreddit(subreddit) {
+  return { type: SELECT_SUBREDDIT, subreddit }
+}
+
+// 更新資料
+export function invaldateSubreddit(subreddit) {
+  return { type: INVALDATE_SUBREDDIT, subreddit }
+}
+
+// 針對subreddit 抓取 posts
+export function requestPosts(subreddit) {
+  return { type: REQUEST_POSTS, subreddit }
+}
+
+// 當網路請求傳回來時，我們會 dispatch RECEIVE_POSTS
+export function receivePosts(subreddit, json) {
+  return {
+    type: RECEIVE_POSTS,
+    subreddit,
+    posts: json.data.children.map(child => child.data),
+    receiveAt: Date.now()
+  }
+}
+
+// thunk action creator
+export function fetchPosts(subreddit) {
+
+  // Thunk middleware 知道如何去處理 function
+  return (dispatch) => {
+    // 呼叫 API
+    dispatch(requestPosts(subreddit))
+
+    return fetch('http://localhost:3000/removed')
+      .then((response) => {
+        return response.json()
+      }).then((json) => {
+        console.log('parsed json', json)
+
+        dispatch(receivePosts(subreddit, json))
+      }).catch((ex) => {
+        console.log('parsing failed', ex)
+      })
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+// import thunkMilldeware from 'react-thunk'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import itemApp from './reducers/index'
